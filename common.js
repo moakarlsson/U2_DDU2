b78aa5c2d190b088794f2f757005ab9fbd7ede52
Moakarlssonns <mo-karlsson@hotmail.com>
2024-12-29 15:56:39 +0100

    Made a button and changes in design

--- common.js
+++ common.js
@@ -2,12 +2,19 @@ let main = document.createElement("main");
 let commonBoxContainer = document.createElement("div");
 let howManyNumbersText = document.createElement("p");
 let input = document.createElement("input");
+input.type = "text";
+
+let createButton = document.createElement ("button");
 document.body.append(main);
 main.append(commonBoxContainer);
 commonBoxContainer.append(howManyNumbersText);
 commonBoxContainer.append(input);
+commonBoxContainer.append(createButton);
 
-commonBoxContainer.style.display = "grid";
+createButton.textContent = "Create";
+createButton.style.marginLeft = "10px"
+createButton.style.padding = "7px";
+
 
 
 howManyNumbersText.textContent = "How Many Numbers In the Grid?";
@@ -16,20 +23,21 @@ howManyNumbersText.style.margin = "30px 10px";
 
 input.style.width = "60px";
 input.style.height = "30px";
+input.style.borderRadius = "3px";
 
 commonBoxContainer.style.display = "flex";
 commonBoxContainer.style.alignItems = "center";
 commonBoxContainer.style.justifyContent = "center";
 
 
-
-function createBoxesForAll(){
+function createBoxesForAll(input){
     let boxContainer = document.createElement("div");
     boxContainer.style.display = "grid";
     boxContainer.style.gridTemplateColumns = "repeat(19, 1fr)";
-    boxContainer.style.gap = "10px";
+    boxContainer.style.gap = "5px";
+    boxContainer.style.marginTop = "70px";
 
-    for (let i = 0; i <= 94; i++){
+    for (let i = 0; i < 95; i++){
         let box = document.createElement("div");
         let randomNumber = Math.floor(Math.random()*90) + 1;
         box.textContent = randomNumber;
@@ -38,8 +46,9 @@ function createBoxesForAll(){
         box.style.justifyContent = "center";
         box.style.alignItems = "center";
         box.style.border = "1px solid black";
-        box.style.borderRadius = "4px";
+        box.style.borderRadius = "10px";
         box.style.height = "50px";
+        box.style.width = "50px"
         box.style.fontSize = "16px";
         box.style.backgroundColor = "white";  // Du kan ändra bakgrundsfärgen här
     
