79e982125a8798e2b7316915ca31122e5ee2d2e5
Moakarlssonns <mo-karlsson@hotmail.com>
2024-12-30 14:14:25 +0100

    Added addEventListener to button

--- common.js
+++ common.js
@@ -15,8 +15,6 @@ createButton.textContent = "Create";
 createButton.style.marginLeft = "10px"
 createButton.style.padding = "7px";
 
-
-
 howManyNumbersText.textContent = "How Many Numbers In the Grid?";
 howManyNumbersText.style.fontFamily = "Helvetica";
 howManyNumbersText.style.margin = "30px 10px";
@@ -24,20 +22,23 @@ howManyNumbersText.style.margin = "30px 10px";
 input.style.width = "60px";
 input.style.height = "30px";
 input.style.borderRadius = "3px";
+input.placeholder = "95";
 
 commonBoxContainer.style.display = "flex";
 commonBoxContainer.style.alignItems = "center";
 commonBoxContainer.style.justifyContent = "center";
 
-
-function createBoxesForAll(input){
-    let boxContainer = document.createElement("div");
+let boxContainer = document.createElement("div");
     boxContainer.style.display = "grid";
     boxContainer.style.gridTemplateColumns = "repeat(19, 1fr)";
     boxContainer.style.gap = "5px";
     boxContainer.style.marginTop = "70px";
+    document.body.appendChild(boxContainer); 
 
-    for (let i = 0; i < 95; i++){
+
+function createBoxesForAll(count){
+    boxContainer.innerHTML = "";
+    for (let i = 0; i < count; i++){
         let box = document.createElement("div");
         let randomNumber = Math.floor(Math.random()*90) + 1;
         box.textContent = randomNumber;
@@ -50,11 +51,15 @@ function createBoxesForAll(input){
         box.style.height = "50px";
         box.style.width = "50px"
         box.style.fontSize = "16px";
-        box.style.backgroundColor = "white";  // Du kan ändra bakgrundsfärgen här
+        box.style.backgroundColor = "white"; 
     
         boxContainer.appendChild(box);
-    }
-    document.body.appendChild(boxContainer);  
+    }  
 }
-createBoxesForAll();
+createBoxesForAll(95);
+
+createButton.addEventListener("click", function(){
+    let count = Number(input.value);
+    createBoxesForAll(count);
+});
 
