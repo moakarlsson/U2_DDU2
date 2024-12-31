1db7c48afcaf4190695b3cd69308b37e914c83ee
Moakarlssonns <mo-karlsson@hotmail.com>
2024-12-31 13:03:06 +0100

    Added HTML to SUM and added content to the website

--- common.js
+++ common.js
@@ -1,40 +1,51 @@
+let numbersContainer = document.getElementById("numbers");
+
+let controlsContainer = document.getElementById("controls");
+let creatorContainer = document.getElementById("creator");
+
 let main = document.createElement("main");
+numbersContainer.appendChild(main);
+
 let commonBoxContainer = document.createElement("div");
-let howManyNumbersText = document.createElement("p");
+commonBoxContainer.style.display = "flex";
+commonBoxContainer.style.alignItems = "center";
+commonBoxContainer.style.justifyContent = "center";
+
 let input = document.createElement("input");
 input.type = "text";
-
-let createButton = document.createElement ("button");
-document.body.append(main);
-main.append(commonBoxContainer);
-commonBoxContainer.append(howManyNumbersText);
-commonBoxContainer.append(input);
-commonBoxContainer.append(createButton);
-
-createButton.textContent = "Create";
-createButton.style.marginLeft = "10px"
-createButton.style.padding = "7px";
-
-howManyNumbersText.textContent = "How Many Numbers In the Grid?";
-howManyNumbersText.style.fontFamily = "Helvetica";
-howManyNumbersText.style.margin = "30px 10px";
-
 input.style.width = "60px";
 input.style.height = "30px";
 input.style.borderRadius = "3px";
 input.placeholder = "95";
 
-commonBoxContainer.style.display = "flex";
-commonBoxContainer.style.alignItems = "center";
-commonBoxContainer.style.justifyContent = "center";
+let createButton = document.createElement ("button");
+document.body.append(main);
+main.append(commonBoxContainer);
+createButton.textContent = "Create";
+createButton.style.marginLeft = "10px"
+createButton.style.padding = "7px";
+
+let howManyNumbersText = document.createElement("p");
+howManyNumbersText.textContent = "How Many Numbers In the Grid?";
+howManyNumbersText.style.fontFamily = "Helvetica";
+howManyNumbersText.style.margin = "30px 10px";
+
+commonBoxContainer.append(howManyNumbersText);
+commonBoxContainer.append(input);
+commonBoxContainer.append(createButton);
+
+creatorContainer.appendChild(commonBoxContainer);
+
 
 let boxContainer = document.createElement("div");
     boxContainer.style.display = "grid";
     boxContainer.style.gridTemplateColumns = "repeat(19, 1fr)";
     boxContainer.style.gap = "5px";
-    boxContainer.style.marginTop = "70px";
+    boxContainer.style.marginTop = "50px";
     document.body.appendChild(boxContainer); 
 
+
+controlsContainer.appendChild(boxContainer);
 
 function createBoxesForAll(count){
     boxContainer.innerHTML = "";
