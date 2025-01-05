f9399ffe69c5235db21fc950d95ffb91c29248a1
Moakarlssonns <mo-karlsson@hotmail.com>
2025-01-05 12:15:26 +0100

    Made buttons for Remove-page

--- common.js
+++ common.js
@@ -1,48 +1,32 @@
 let numbersContainer = document.getElementById("numbers");
-let controlsContainer = document.getElementById("controls");
 let creatorContainer = document.getElementById("creator");
-
 let main = document.createElement("main");
-
-
 let commonBoxContainer = document.createElement("div");
-commonBoxContainer.style.display = "flex";
-commonBoxContainer.style.alignItems = "center";
-commonBoxContainer.style.justifyContent = "center";
+commonBoxContainer.classList.add("flex-center");
 
 let input = document.createElement("input");
 input.type = "text";
 input.style.width = "60px";
 input.style.height = "30px";
 input.style.borderRadius = "3px";
-input.placeholder = "95";
+input.style.padding = "5px";
+input.style.fontSize = "16px";
 
 let createButton = document.createElement ("button");
 document.body.append(main);
 main.append(commonBoxContainer);
 createButton.textContent = "Create";
-createButton.style.marginLeft = "10px"
-createButton.style.padding = "7px";
 
 let howManyNumbersText = document.createElement("p");
 howManyNumbersText.textContent = "How Many Numbers In the Grid?";
-howManyNumbersText.style.fontFamily = "Helvetica";
-howManyNumbersText.style.margin = "30px 10px";
 
-commonBoxContainer.append(howManyNumbersText);
-commonBoxContainer.append(input);
-commonBoxContainer.append(createButton);
+commonBoxContainer.append(howManyNumbersText, input, createButton);
 creatorContainer.appendChild(commonBoxContainer);
 
-
-
 let boxContainer = document.createElement("div");
-    boxContainer.style.display = "grid";
-    boxContainer.style.gridTemplateColumns = "repeat(19, 1fr)";
-    boxContainer.style.gap = "5px";
-    boxContainer.style.marginTop = "70px";
-    numbers.appendChild(boxContainer); 
-    let boxesArray = [];
+boxContainer.classList.add("grid-container");
+numbersContainer.appendChild(boxContainer); 
+let boxesArray = [];
 
 numbers.appendChild(boxContainer);
 
@@ -51,14 +35,12 @@ function createBoxesForAll(count){
     boxesArray = [];
     for (let i = 0; i < count; i++){
         let box = document.createElement("div");
-        let randomNumber = Math.floor(Math.random()*90) + 1;
+        let randomNumber = Math.floor(Math.random()*99) + 1;
         box.textContent = randomNumber;
         box.classList.add("boxStyle")
         boxContainer.appendChild(box);
         boxesArray.push(box);
-    }  
-    
-    
+    }   
 }
 createBoxesForAll(95);
 
@@ -66,5 +48,3 @@ createButton.addEventListener("click", function(){
     let count = Number(input.value);
     createBoxesForAll(count);
 });
-
-
--- sum/index.js
+++ sum/index.js
@@ -2,7 +2,7 @@
 const sumAllDOM = document.getElementById("sum_Result_All");
 const sumMarkedDOM = document.getElementById("sumResultMarked");
 const resetButton = document.getElementById("reset");
-
+//Funktion som räknar ut summan av alla boxar med random nummer i
 function sumOfAll(){
     let totalSum = 0;
     for(let box of boxesArray){
@@ -10,6 +10,8 @@ function sumOfAll(){
     }
     sumAllDOM.textContent = totalSum;
 }
+//Funktion som räknar ut summan av alla nummerboxar som användaren markerar.
+
 function sumOfMarked() {
     let markedSum = 0;
     for (let box of boxesArray) {
@@ -17,9 +19,9 @@ function sumOfMarked() {
             markedSum += Number(box.textContent);
         }
     }
-    sumMarkedDOM.textContent = markedSum;
+    sumMarkedDOM.textContent = markedSum; 
 }
-
+//Event som är kopplat till funktionen SumOfMarked som sätter en klass på boxarna när det klickas på.
 function ClickEventBoxes() {
     for (let box of boxesArray) {
         box.addEventListener("click", function () {
@@ -29,6 +31,7 @@ function ClickEventBoxes() {
     }
 }
 
+//Event som gör att resetKnappen clearar boxarna och summan.
 resetButton.addEventListener("click", function () {
     for (let box of boxesArray) {
         box.classList.remove("marked");
@@ -36,7 +39,7 @@ resetButton.addEventListener("click", function () {
     }
     sumMarkedDOM.textContent = "";
 });
-
+//
 createButton.addEventListener("click", function () {
     let count = Number(input.value);
     createBoxesForAll(count);
